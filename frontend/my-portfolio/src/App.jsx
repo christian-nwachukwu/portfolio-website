36d7956c2bf2b15d7958b6c483b780b53c98dc30
Christian Nwachukwu <christian.nwachukwu2020@gmail.com>
2025-01-04 18:01:23 +0100

    Installed react-router-dom to be able to link to all pages for navigation. implementing Layout and Outlet

--- frontend/my-portfolio/src/App.jsx
+++ frontend/my-portfolio/src/App.jsx
@@ -1,12 +1,56 @@
-import { useState } from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Portfolio from './pages/Portfolio'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+
+// For pages/routes that requires inside the html body, Navbar, Footer, Sections, Hero etc
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Layout/>,
+  children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+
+      {
+        path: "/about",
+        element: <About/>
+      },
+
+      {
+        path: "/portfolio",
+        element: <Portfolio/>
+      },
+  ]
+},
+])
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>Portfolio Page</div>
+       <RouterProvider router = {router}/>
     </>
   )
 }
